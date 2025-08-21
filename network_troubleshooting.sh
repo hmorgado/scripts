5b2a55b8c6fa68509ba04996e237e99f2da0e324
Heitor Morgado <heitor.morgado@skipthedishes.ca>
2025-08-21 14:50:49 -0600

    * main - better code

--- network_troubleshooting.sh
+++ network_troubleshooting.sh
@@ -6,24 +6,27 @@ FQDN=$1
 
 check_dns_resolution () {
   echo
-  host "$FQDN" > /dev/null && echo "Successfully resolved $FQDN"
+  host "$FQDN" &> /dev/null && \
+  echo "Successfully resolved $FQDN" || \
+  echo "Could not resolv $FQDN"
 }
 
 check_reachability () {
   echo
   echo "pinging $FQDN"
-  ping "$FQDN" -c 3 > /dev/null && echo "Successfully pinged $FQDN"
+  ping "$FQDN" -c 3 &> /dev/null \
+  && echo "Successfully pinged $FQDN" || \
+  echo "Could not ping $FQDN"
 }
 
 check_tcp () {
   echo
-  nc -z "$FQDN" 443 && echo "Reached port 443 via TCP at $FQDN"
+  nc -z "$FQDN" 443 &> /dev/null && \
+  echo "Reached port 443 via TCP at $FQDN" || \
+  echo "Could not reach port 443 at $FQDN"
 }
 
-if [[ ! $(check_dns_resolution) ]]; then
-  echo "DNS resolution failed, skipping TCP test"
-  check_reachability
-else
-  check_tcp
-fi
+check_dns_resolution
+check_reachability
+check_tcp
 
